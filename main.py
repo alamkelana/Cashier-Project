4d46ffed14e74164d27915318e59e6c2d80cec6a
dell <alamkelanalubis@gmail.com>
2023-01-26 13:26:31 +0700

    update 26 Jan 2023

--- main.py
+++ main.py
@@ -1,20 +1,43 @@
 import project
 
 dict_item = {}
-print("Welcome!")
+print("\n..........Welcome!..........")
 
 while True:
-    print("\nMAIN MENU")
-    print("1. Input item \n2. Update item \n3. Delete item \n4. Check order \n5. Checkout your order\n")
-    menu = int(input("What would you like to do? (input a number based on the menu above): ").strip())
-    print("")
-    if menu == 1:
-        dict_item = project.input_item()
-    elif menu == 2:
-        dict_item = project.update_item()
-    elif menu == 3:
-        dict_item = project.delete_item()
-    elif menu == 4:
-        project.check_order()
-    elif menu == 5: 
-        project.total_price()
+    try:
+        print("\nMAIN MENU")
+        print("---------")
+        print("1. Input item \n2. Update item \n3. Delete item \n4. Check order \n5. Checkout your order\n6. Exit\n")
+        menu = int(input("What would you like to do? (input a number based on the menu above): ").strip())
+        print("----------------------------------------------------------------------")
+        if menu == 1:
+            dict_item = project.input_item()
+        elif menu == 2:
+            if dict_item == {}:
+                print("\nThere's no item to be updated")
+            else: 
+                project.check_order()
+                print("")
+                dict_item = project.update_item()
+        elif menu == 3:
+            if dict_item == {}:
+                print("\nThere's no item to be deleted")
+            else: dict_item = project.delete_item()
+        elif menu == 4:
+            if dict_item == {}:
+                print("\nThere's no order yet!")
+            else: project.check_order()
+        elif menu == 5: 
+            if dict_item == {}:
+                print("\nThere's no order yet!")
+            else: 
+                project.check_order()
+                print("")
+                project.total_price()
+                print("\nThank you for coming!\n")
+                break
+        elif menu == 6:
+            print("\nThank you for coming!\n")
+            break 
+    except:
+        print("\nPlease only input number based on the menu above")
--- project.py
+++ project.py
@@ -11,76 +11,85 @@ def input_item():
             jumlah_item = int(input("Input total item: ").strip())
             harga_item = int(input("Input item price: ").strip())
             dict_item[nama_item] = [jumlah_item, harga_item]
+            print("----------------------------------------------------------------------")
             yn = input("Input another?? (Y/N): ").strip().lower()
             if yn == "y":
                 continue
             elif yn == "n":
                 break
             else:
-                print("\nWrong Input!!!")
+                print("\nPlease only input Y/N")
                 break
+        print("\nThe items was added successfully")
         return dict_item
     except:
-        print("\nWrong input!!!")
+        print("\nPlease only input number to 'total item' and 'item price'")
     
 # fungsi update item, ada 3 opsi: update item, update jumlah, update harga
 def update_item():
     global dict_item
     print("1. Update item name \n2. Update total item \n3. Update item price ")
-    update = int(input("Choose update type: ").strip())
+    update = int(input("Choose update type (input a number based on the menu above): ").strip())
     if update == 1:
         update_item = input("Input item name that you'd like to update: ").strip().title()
         if update_item in dict_item.keys():
             updated_item = input("Input new item name: ").strip().title()
             dict_item[updated_item] = dict_item[update_item]
             del dict_item[update_item]
+            print("\nThe item name was updated successfully")
         else: 
-            print(f"No present item with name {update_item}")
+            print(f"\nNo present item with name {update_item}")
     elif update == 2:
         update_jumlah = input("Input item name that you'd like to update: ").strip().title()
         if update_jumlah in dict_item.keys():
             updated_jumlah = int(input("Input new total item: ").strip())
             dict_item[update_jumlah][0] = updated_jumlah
-        else: print(f"No present item with name {update_jumlah}")
+            print("\nTotal item was updated successfully")
+        else: print(f"\nNo present item with name {update_jumlah}")
     elif update == 3:
         update_harga = input("Input item name that you'd like to update: ").strip().title()
         if update_harga in dict_item.keys():
             updated_harga = int(input("Input new item price: ").strip())
             dict_item[update_harga][1] = updated_harga
-        else: print(f"No present item with name {update_harga}")
-    else: print("\nWrong Input!")
+            print("\nThe item price was updated successfully")
+        else: print(f"\nNo present item with name {update_harga}")
+    else: print("\nPlease only input number based on the menu above")
     return dict_item
 
 # fungsi delete item, ada 2 opsi: delete item pake nama, reset transaction apus semua
 def delete_item():
     global dict_item
-    print("1. Delete item \n2. Reset all transaction")
-    delete = int(input("Choose delete type: ").strip())
+    print("1. Delete an item \n2. Reset all transaction")
+    delete = int(input("Choose delete type (input a number based on the menu above): ").strip())
     if delete == 1:
         item_delete = input("Input item name: ").strip().title()
         if item_delete in dict_item.keys():
             del dict_item[item_delete]
+            print("\nThe item was deleted successfully")
         else: print(f"No present item with name {item_delete}")
     elif delete == 2:
         dict_item = {}
-    else: print("\nWrong input!")
+        print("\nAll items was deleted successfully")
+    else: print("\nPlease only input number based on the menu above")
     return dict_item
 
 
 # fungsi check order, pemesanan benar kalo order sudah lengkap, pemesanan
 # belum benar kalau order ada yg kosong, keluarin output hasil transaksi dlm bentuk tabel
 def check_order():
+    print("Order Details: \n")
     print("{:<15} {:<15} {:<15} {:<15}".format("Item Name", "Total Item", "Item Price", "Total"))
     for key in dict_item.keys():
         print("{:<15} {:<15} {:<15} {:<15}".format(key, dict_item[key][0], dict_item[key][1],dict_item[key][0]*dict_item[key][1]))
+    print("----------------------------------------------------------------------")
 
 # fungsi total price, tambahin branching ketentuan diskon
 def total_price():
     total = 0
     for key in dict_item.keys():
         total = total+(dict_item[key][0]*dict_item[key][1])
-    print(f'Your total is {total}')
-    if total > 20000 and total <=300000:
+    print(f'Your total is Rp {total}')
+    if total > 200000 and total <=300000:
         diskon = 5
         total = total*(100-diskon)/100
     elif total > 300000 and total <=500000:
@@ -92,4 +101,5 @@ def total_price():
     else: 
         diskon = 0
         total = total
-    print(f'You get a {diskon}% discount, you have to pay Rp {total}')
+    print(f'You get {diskon}% discount, you have to pay Rp {int(total)}')
+
